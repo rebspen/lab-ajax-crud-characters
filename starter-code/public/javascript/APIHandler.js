64e0d47d6b48705db6f7837f8444a35f0cfa687c
Rebecca Spence <Bec@Rebeccas-MacBook-Pro.local>
2019-11-22 15:16:28 +0000

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,43 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = "http://localhost:8000";
   }
 
   getFullList () {
-
+    return axios
+    .get(this.BASE_URL + '/characters')
+    .then(response => Promise.resolve(response.data));
+    
   }
 
   getOneRegister () {
-
+    return axios
+    .get(this.BASE_URL + `/characters/${id}`)
+    .then(response => Promise.resolve(response.data));
+    
   }
 
   createOneRegister () {
-
+    return axios
+    .post(this.BASE_URL + '/character', {
+        ...data,
+        id: Math.floor(Math.random() * 100000)
+    })
+    .then(response => Promise.resolve(response.data));
+    
   }
 
   updateOneRegister () {
-
+    return axios
+    .delete(this.BASE_URL + `/character/${id}`)
+    .then(response => Promise.resolve(response.data));
+    
   }
 
   deleteOneRegister () {
-
+    return axios
+    .patch(this.BASE_URL + `/cities/${id}`, data)
+    .then(response => Promise.resolve(response.data));
+    
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,8 +1,48 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+const $container = document.querySelector(".character-info");
 
+
+//DOM find by id
+const $inputSearchId = document.querySelector("input[name=character-id]");
+// DOM delete
+const $inputDeleteId = document.querySelector("input[name=character-id-delete]");
+const $deleteButton =  document.querySelector("#delete-one");
+//DOM new character form
+const $newInputName = document.querySelector("#new-character-form [name=name]");
+const $newInputOccupation = document.querySelector("#new-character-form [name=occupation]");
+const $newInputWeapon = document.querySelector("#new-character-form [name=weapon]");
+const $newInputCartoon = document.querySelector("#new-character-form [name=cartoon]");
+const $newButton = document.querySelector("#new-character-form #new-data");
+//DOM edit character form
+const $inputId = document.querySelector("#edit-character-form [name=chr-id]");
+const $inputName = document.querySelector("#edit-character-form [name=name]");
+const $inputOccupation = document.querySelector("#edit-character-form [name=occupation]");
+const $inputWeapon = document.querySelector("#edit-character-form [name=weapon]");
+const $inputCartoon = document.querySelector("#edit-character-form [name=cartoon]");
+const $updateButton = document.querySelector("#edit-character-form #update-data");
+window.addEventListener('load', () => {
+  
+  // cRud --- READ in CRUD operations
+  document.getElementById('fetch-all').addEventListener('click', function (event) {
+    console.log(event)
+    console.log(charactersAPI.getFullList());
+    charactersAPI.getFullList()
+    .then(characters => {
+      $container.innerHTML = ""; // Remove all the content of the container
+      for (let i = 0; i < characters.length; i++) {
+        $container.innerHTML += `
+        <div class="character-info">
+          <div class="name"><p><b>Name</b>: ${characters[i].name}</p></div>
+          <div class="occupation"><p><b>Occupation</b>: ${characters[i].occupation}</p></div>
+          <div class="cartoon"> <p><b>Is a Cartoon</b>:${
+            characters[i].cartoon ? "Cartoon" : "Not a cartoon"
+          }</p></div>
+          <div class="weapon"><p><b>Weapon</b>: ${characters[i].weapon}</p></div>
+        </div>
+        `;
+      }
+    });
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
